0e56860b28d25f815294b45efd3ac10367ed3010
Bushra-Memon <memonbusu14@gmail.com>
2025-12-18 14:12:31 +0530

    Add Day1 JS file.

---
+++ tasks1.js
@@ -0,0 +1,39 @@
+//1
+const firstname =  'Bushra';
+const middlename = 'Yunusbhai';
+const lastname =  'Memon';
+const age = 20;
+
+console.log("firstname =" ,firstname);
+console.log("middlename =" ,middlename);
+console.log("lastname =" ,lastname);
+console.log("age=" ,age);
+
+//2
+x = "25";
+console.log(typeof x);
+a = Number(x) + 10;
+console.log(a);
+
+//3
+a = 10;
+b = 20;
+console.log("Value of a before swapping:",a);
+console.log("Value of b before swapping:",b);
+[a,b] = [b,a];
+console.log("Value of a after swapping:",a);
+console.log("Value of b after swapping:",b);
+
+//4
+x = 10;
+console.log(x == 8);
+console.log(x != 10);
+console.log(x == 10);
+console.log(x <= 10);
+console.log(x >= 10);
+
+
+
+
+
+
